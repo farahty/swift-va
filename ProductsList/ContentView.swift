17b4cf99c0f6329e1732f0295638d7f553f7d0a7
Nimer Farahty <nimer.farahty@gmail.com>
2020-09-22 13:50:07 +0300

    Initial Commit

---
+++ ProductsList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProductsList
+//
+//  Created by Nimer Farahty on 9/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProductsList/ProductsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsListApp.swift
+//  ProductsList
+//
+//  Created by Nimer Farahty on 9/22/20.
+//
+
+import SwiftUI
+
+@main
+struct ProductsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
